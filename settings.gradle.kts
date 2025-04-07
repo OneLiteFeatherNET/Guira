4d03201885910b9fe57b69a3474a8224ac0f2fda
theEvilReaper <steffenwx@gmail.com>
2025-04-07 18:18:55 +0200

    Update dependency section

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,36 @@
 rootProject.name = "Guira"
 
+dependencyResolutionManagement {
+    repositories {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        mavenCentral()
+        maven("https://reposilite.worldseed.online/public")
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                }
+            } else {
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+    versionCatalogs {
+        create("libs") {
+            version("aves", "1.6.1")
+            version("junit", "5.12.1")
+
+            library("aves", "de.icevizion.lib", "aves").versionRef("aves")
+
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+        }
+    }
+}
+
