973eb011539e124e673aff899ee97ed4bed21db6
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2025-07-19 21:58:53 +0200

    Add functional interfaces (#21)
    * Add functional interfaces
    
    * Update functional interfaces to reflect latest changes
    
    ---------
    
    Co-authored-by: theEvilReaper <theEvilReaper@users.noreply.github.com>

---
+++ src/main/java/net/onelitefeather/guira/functional/OptionalSetupGetter.java
@@ -0,0 +1,27 @@
+package net.onelitefeather.guira.functional;
+
+import net.onelitefeather.guira.data.SetupData;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Functional interface for getting setup data associated with a specific {@link UUID}.
+ *
+ * @author theEvilReaper
+ * @version 0.1.0
+ * @since 0.3.0
+ */
+@FunctionalInterface
+public interface OptionalSetupGetter {
+
+    /**
+     * Gets the setup data for the given {@link UUID}.
+     *
+     * @param uuid the {@link UUID} of the player
+     * @return an Optional containing the setup data if it exists, or an empty Optional if it does not
+     */
+    @NotNull Optional<@Nullable SetupData> get(@NotNull UUID uuid);
+}
